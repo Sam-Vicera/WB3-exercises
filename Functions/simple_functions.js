60381ef140faf66b6b4870fa9995a1df0cb3889c
Sam-Vicera <shernandezvicera@gmail.com>
2023-10-24 13:34:11 -0700

    Exercise 1 complete

---
+++ Functions/simple_functions.js
@@ -0,0 +1,22 @@
+
+function favoriteThings(){
+console.log("My name is Samuel Hernandez Vicera");
+console.log("My current favorite movie is La La Land");
+console.log("My favorite artist is The Weeknd");
+console.log()
+}
+
+function whyImHere(){
+    console.log("I joined the learn to code program as a means to broaden my skills and to get a career in something I can enjoy and grow in.");
+    console.log()
+}
+
+function favoritePlace(){
+console.log("My favorite place to visit is the beach. I don't go often but it's always a nice experience when I do.");
+console.log()
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
+
